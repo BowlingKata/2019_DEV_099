a6fcfa87997e8474cf0d4bcd3ba020446d1db428
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-02-14 01:05:21 +0100

    Added UI testing to test just the expected result into a UILabel and the showing of an UIAlert in case of invalid input

--- BowlingTest/BowlingTestUITests/BowlingTestUITests.swift
+++ BowlingTest/BowlingTestUITests/BowlingTestUITests.swift
@@ -8,27 +8,47 @@
 
 import XCTest
 
-class BowlingTestUITests: XCTestCase {
+class BowlingTestUITests: XCTestCase
+{
+  var app: XCUIApplication!
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUp()
+  {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+    app = XCUIApplication()
+    app.launch()
+  }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  func testValidInput()
+  {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    let nameTextField = app.textFields["inputField"]
+    nameTextField.tap()
+    nameTextField.typeText("X X X X X X X X X X X X")
 
+    app.buttons["Calculate"]/*@START_MENU_TOKEN@*/.tap()/*[[".tap()",".press(forDuration: 0.5);"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+
+    XCTAssert(XCUIApplication().staticTexts["300"].exists)
+  }
+
+  func testNotValidInput()
+  {
+    let nameTextField = app.textFields["inputField"]
+    nameTextField.tap()
+    nameTextField.typeText("X X X X X X X X X")
+
+    app.buttons["Calculate"]/*@START_MENU_TOKEN@*/.tap()/*[[".tap()",".press(forDuration: 0.5);"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+
+    XCTAssert(app.alerts["Error"].buttons["Close"].exists)
+  }
 }
